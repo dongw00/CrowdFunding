8f949e1063b52f49939ab53d4f3d2fbe0eefcd02
ParkJinwoo <pjw9195@naver.com>
2019-10-08 01:13:25 +0900

    react setting 2

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,16 +1,15 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AppLayout from "./components/AppLayout"
 
-function App() {
+function App({Component}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-
-      </header>
-    </div>
+      <AppLayout>
+        <Component />
+      </AppLayout>
   );
 }
+
 
 export default App;
